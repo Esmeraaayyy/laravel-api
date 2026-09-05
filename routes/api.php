8f3446306ead0c627ccf5b73dce676d229efde71
Esmeraaayyy <dejesusharkie@gmail.com>
2026-09-05 23:52:06 +0800

    Add API authentication with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AuthController;
 
-Route::apiResource('students', StudentController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/students', [StudentController::class, 'index']);
+});
